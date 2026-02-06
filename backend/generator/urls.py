0634fc36063e8bf48a24615b0dde86d94da8b1bf
Sanjay Marathi <sanjaymarathiiii@gmail.com>
2026-02-06 17:06:14 +0530

    More Accurate Documention using wikipedia

--- backend/generator/urls.py
+++ backend/generator/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import generate_documentation, download_pdf
+from .views import generate_documentation, download_pdf, connection_status
 
 urlpatterns = [
     path("generate/", generate_documentation),
     path("pdf/", download_pdf),
+    path("status/", connection_status),
 ]
